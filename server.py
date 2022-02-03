33e622fd0d26904dce39cce2dfc111e801efc1c4
Thaddeus <watkins7@umbc.edu>
2022-02-02 19:53:13 -0500

    server.py  -Added logging  -Finished BaseRequestHandler child class __init__  -Finished TCPServer child class __init__

--- server.py
+++ server.py
@@ -1,30 +1,108 @@
+# Used to setup server handler
 import socketserver
+
+# Used to get serverIP
 import socket
 
+# Used to setup logging
+import logging
+import logging.config
 
+# Used to display logging to stdout
+import sys
+
+# Used for Current Time
+from datetime import datetime
+
+# fileServer child Class, whose super class is socketServer.TCPServer
+class fileServer(socketserver.TCPServer):
+
+    # __init__ ("fileServer" child class)
+    def __init__(self, serverIP, childHandler):
+
+        #######################################
+        # Set Logging Objects
+        #######################################
+
+        # Set logging file
+        #logging.config.fileConfig('fileServer.log')
+
+        # Set logging configuration
+        logging.basicConfig(filename='fileServer.log', level=logging.DEBUG, filemode='w',
+        format='%(asctime)s\t\tLogger: %(name)s\t\tLevel: %(levelname)s\t\tEvent: %(message)s',
+        datefmt='%Y:%m:%d %H:%M:%S')
+
+        # Create Logger
+        self.serverLog = logging.getLogger("Server")
+
+        # Create Handler, set level to at least DEBUG
+        loggingHandler = logging.StreamHandler(sys.stdout)
+        self.serverLog.addHandler(loggingHandler)
+
+        # Post to log, "File Server Started"
+        date = datetime.now().strftime("DATE: %Y:%m:%d\tTIME: %H:%M:%S\t\tEVENT: ")
+        print(date, end='')
+        self.serverLog.debug("File Server Started")
+
+
+        # Call Super Class "TCPServer", to __init__ Child TCPServer Class
+        socketserver.TCPServer.__init__(self, serverIP, childHandler)
+        return
+
+# fileServerHandler child class, whose super class is socketserver.BaseRequestHandler
 # Class instance to handle server requests
-# Must pass "socketserver"."BaseRequestHandler" to be instantiated once per connection
 class fileServerHandler(socketserver.BaseRequestHandler):
 
-    def __init__(self):
-        print('File Server is up\n')
+    # __init__ ("fileServerHandler" child class)
+    def __init__(self, requestType, clientIP, serverIP):
 
-    # NEEDED, handle() method must be implemented to override the super class
+        #######################################
+        # Set Logging Objects
+        ##################
+
+        # Set logging file
+        #logging.config.fileConfig('fileServer.log')
+
+        # Set logging configuration
+        logging.basicConfig(filename='fileServer.log', level=logging.DEBUG, filemode='w',
+        format='%(asctime)s\t\tLogger: %(name)s\t\tLevel: %(levelname)s\t\tEvent: %(message)s',
+        datefmt='%Y:%m:%d %H:%M:%S')
+
+        # Create serverHandler Logger
+        self.serverHandlerLog = logging.getLogger("Server Handler")
+
+        # Create Handler
+        loggingHandler = logging.StreamHandler(sys.stdout)
+        self.serverHandlerLog.addHandler(loggingHandler)
+
+        # Post to log, "File Server Started"
+        print(datetime.now().strftime("DATE: %Y:%m:%d\tTIME: %H:%M:%S\t\tEVENT: "), end='')
+        self.serverHandlerLog.info("File Handler Started")
+
+        # Call Super Class "BaseRequestHandler", to __init__ Child Handler Class
+        socketserver.BaseRequestHandler.__init__(self, requestType, clientIP, serverIP)
+        return
+
+
+    # NEEDED!!!
+    # handle() method must be implemented to override the super class
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        data = self.request.recv(1024).strip()
+
         # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        self.request.sendall(data.upper())
+        return
 
 
 if __name__ == '__main__':
+
     # Set Server IP
     serverHost = socket.gethostname()
     serverIP = socket.gethostbyname(serverHost)
 
     # Set Port Number
-    port = 50000
+    serverPort = 50000
 
-    fileServer = socketserver.TCPServer((serverIP, port), fileServerHandler)
-    print('File Server is up\n')
+    # Make fileServer() Object, with (socket) and fileServerHandler
+    startFileServer = fileServer((serverIP, serverPort), fileServerHandler)
+
